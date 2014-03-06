2ea2b0a6eabea076277e7fc6fd384e7b40edce1e
donayam <donayam.nega@dventus.com>
2014-03-06 13:48:24 +0300

    Added some console io

--- src/com/dventus/sm502/Hello.java
+++ src/com/dventus/sm502/Hello.java
@@ -1,9 +1,16 @@
 package com.dventus.sm502;
 
+import java.util.Scanner;
 public class Hello {
 
 	public static void main()
 	{
+		int x=0;
+	    Scanner s = new Scanner(System.in);
+	    x= s.nextInt();
+	    
 		System.out.println("This is my first java git project.");
+		
+		System.out.println("Input form console is:"+x);
 	}
 }
